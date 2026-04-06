f27d56551cf9555ad70d75c3ed030297f86eae13
Qing-Yun-Dev <3828086635@qq.com>
2026-04-06 15:47:38 +0800

    更新 Vape.lua

--- Vape.lua
+++ Vape.lua
@@ -1600,8 +1600,8 @@ function UI:Window(text, preset, closebind)
             TextBox.TextSize = 15.000
 
             TextBox.FocusLost:Connect(
-                function(ep)
-                    if ep then
+                function(KeyInput)
+                    if KeyInput then
                         if #TextBox.Text > 0 then
                             pcall(callback, TextBox.Text)
                             if disapper then
